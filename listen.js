955d30f574377200b1046b60d79edea8eaf04eda
alexcupit <alexcupit@outlook.com>
2022-12-21 11:33:38 +0000

    adds mongo connection before listening

--- listen.js
+++ listen.js
@@ -1,4 +1,8 @@
 const app = require('./app');
+const db = require('./db/connection');
 
 const { PORT = 9090 } = process.env;
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
+db.then(() => {
+  app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+});
