478cf6beed65ebfd4daeab55eb907c50561094ac
zakaria552 <zakaria.farah55@outlook.com>
2022-12-22 15:33:05 +0000

    added test data for users and leaderboard

---
+++ db/data/test-data/test-leaderboard.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        date: "2022-12-22",
+        members: [
+            {username: "zakaria", todayStats: {date: "2022-12-22", score: 3, timeTaken: "200", correctAns: 2}},
+            {username: "alex", todayStats: {date: "2022-12-22", score: 6, timeTaken: "300", correctAns: 5}}, 
+            {username: "fel", todayStats: {date: "2022-12-22", score: 4, timeTaken: "400", correctAns: 3}}, 
+            {username: "dan", todayStats: {date: "2022-12-22", score: 5, timeTaken: "500", correctAns: 4}}
+        ], // [{username, todayStats}]
+        leaderboardName: "global",
+    },
+    {
+        date: "2022-12-22",
+        members: [
+            {username: "zakaria", todayStats: {date: "2022-12-22", score: 3, timeTaken: "200", correctAns: 2}},
+            {username: "alex", todayStats: {date: "2022-12-22", score: 6, timeTaken: "300", correctAns: 5}}, 
+        ], // [{username, todayStats}]
+        leaderboardName: "quizNight",
+    }
+]
