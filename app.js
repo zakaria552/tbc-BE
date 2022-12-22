24d26bce28469fd027621ee9b44e8ea5702edfff
zakaria552 <zakaria.farah55@outlook.com>
2022-12-22 14:05:25 +0000

    added cors

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { handleCustomErrors, handleDefaultErrors } = require("./errors");
 const apiRouter = require("./routes/api.router");
+const cors = require("cors")
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 
 app.use("/api", apiRouter);
-
 // Error handling
 
 app.all("/*", (req, res) => {
