21677c6e2fc06d56f48a7d1dcac123d82f500f11
zakaria552 <zakaria.farah55@outlook.com>
2022-12-23 15:25:19 +0000

    added costume mongodb error handler

--- db/schemas/usersSchema.js
+++ db/schemas/usersSchema.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const usersSchema = mongoose.Schema({
-  userId: {type: String,required: true},
-  username: {type: String,required: true},
+  userId: {type: String, required: true},
+  username: {type: String, required: true},
   currentStreak: Number,
   highestScore: Number,
   dateLastPlayed: String,
--- errors/index.js
+++ errors/index.js
@@ -7,6 +7,8 @@ exports.handleMongoDbErrors = (err, req, res, next) => {
   Object.keys(err.errors).forEach((errField) => {
     if (err.errors[errField].kind === "required") {
       res.status(400).send({ msg: "missing required field" });
+    } else if (err.errors[errField].valueType === "Array" || err.errors[errField].valueType === "Object") {
+      res.status(400).send({ msg: "wrong data type for required field" });
     } else {
       next(err)
     }
