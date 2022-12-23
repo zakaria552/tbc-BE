acfa768fe3257e77f1a24cbc462688660288e86f
zakaria552 <zakaria.farah55@outlook.com>
2022-12-23 15:19:23 +0000

    handled errors

--- errors/index.js
+++ errors/index.js
@@ -3,7 +3,15 @@ exports.handleCustomErrors = (err, req, res, next) => {
     res.status(err.status).send({ msg: err.msg });
   } else next(err);
 };
-
+exports.handleMongoDbErrors = (err, req, res, next) => {
+  Object.keys(err.errors).forEach((errField) => {
+    if (err.errors[errField].kind === "required") {
+      res.status(400).send({ msg: "missing required field" });
+    } else {
+      next(err)
+    }
+  })
+}
 exports.handleDefaultErrors = (err, req, res, next) => {
   console.log(err, "UNHANDLED ERROR");
   res.status(500).send("Server error");
