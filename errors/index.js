60a47695fa68a3790fcd707e8ed3d7b957fed338
zakaria552 <zakaria.farah55@outlook.com>
2022-12-26 12:53:50 +0000

    added some tests and custome error handling

--- errors/index.js
+++ errors/index.js
@@ -1,20 +1,30 @@
 exports.handleCustomErrors = (err, req, res, next) => {
   if (err.status && err.msg) {
-    console.log(err);
     res.status(err.status).send({ msg: err.msg });
   } else next(err);
 };
 exports.handleMongoDbErrors = (err, req, res, next) => {
-  console.log(err)
-  Object.keys(err.errors).forEach((errField) => {
-    if (err.errors[errField].kind === "required") {
+  // patch
+  if(err.valueType === "Array" || err.valueType === "Object" ) {
+    res.status(400).send({ msg: "wrong data type for required field" })
+  }
+  // post
+  const errors = Object.keys(err.errors)
+  for(let i = 0; i < errors.length; i++) {
+    if (err.errors[errors[i]].kind === "required") {
       res.status(400).send({ msg: "missing required field" });
-    } else if (err.errors[errField].valueType === "Array" || err.errors[errField].valueType === "Object") {
+      break;
+    } else if (err.errors[errors[i]].valueType === "Array" || err.errors[errors[i]].valueType === "Object") {
       res.status(400).send({ msg: "wrong data type for required field" });
+      break
     } else {
       next(err)
+      break
     }
-  })
+  }
+  // Object.keys(err.errors).forEach((errField) => {
+  // })
+  
 }
 exports.handleDefaultErrors = (err, req, res, next) => {
   console.log(err, "UNHANDLED ERROR");
