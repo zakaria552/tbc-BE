ebd6fd0818ad20d7ce089ade4a6b3d2e5b2c4921
zakaria552 <zakaria.farah55@outlook.com>
2022-12-26 12:51:47 +0000

    api route now responds with json of all endpoints

--- routes/users.router.js
+++ routes/users.router.js
@@ -2,6 +2,7 @@ const {
   getUserById,
   deleteUserByUserId,
   postUser,
+  patchUser,
 } = require("../controllers/users.controller");
 
 const usersRouter = require("express").Router();
@@ -10,6 +11,7 @@ usersRouter.route("/:userId").get(getUserById);
 usersRouter.route("/:userId").delete(deleteUserByUserId);
 
 usersRouter.route("/").post(postUser)
+usersRouter.route("/:userId").patch(patchUser)
 
 module.exports = usersRouter
 
